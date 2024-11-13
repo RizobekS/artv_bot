969b073a17a6f77f4fea5bc6192100b564405979
RizobekS <rizobeksaibov@gmail.com>
2024-11-13 21:20:52 +0500

    added function notification from new registered user

--- registration/admin.py
+++ registration/admin.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from django.urls import path
 from django.template.response import TemplateResponse
 from django.utils.html import strip_tags
-from telegram import Bot, ParseMode
+from telegram import Bot
 
 from .management.commands.send_message import send_message_to_paid_participants
 from .models import Participant, AuctionMessage
@@ -58,7 +58,7 @@ def send_latest_auction_message():
             print("Нет доступных сообщений для отправки.")
             return
 
-        message_text = message.content  # Оставляем HTML или Markdown теги
+        message_text = strip_tags(message.content)  # Убираем HTML, если ваш бот не поддерживает HTML-разметку
 
         # Отправка сообщения только оплатившим участникам
         bot = Bot(token=TELEGRAM_TOKEN)
@@ -69,8 +69,7 @@ def send_latest_auction_message():
 
         for participant in paid_participants:
             try:
-                # Указываем режим разметки HTML или MarkdownV2
-                bot.send_message(chat_id=participant.chat_id, text=message_text, parse_mode=ParseMode.HTML)
+                bot.send_message(chat_id=participant.chat_id, text=message_text)
                 print(f"Сообщение отправлено участнику: {participant.name}")
             except Exception as e:
                 print(f"Ошибка при отправке для {participant.name}: {e}")
